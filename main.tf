677ca6f9756591ebf2f1fbb7c34b7a9106f7c198
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2024-08-19 18:38:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags                 = var.tags
+}
+
+resource "aws_subnet" "this" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.subnet_cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  tags                    = var.tags
+}
