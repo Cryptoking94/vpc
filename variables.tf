eb57c5e1d88760a1b5518836170e1b3b482662df
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2024-08-19 18:40:12 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "region" {
+  description = "The AWS region to create the VPC in"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "subnet_cidr_block" {
+  description = "The CIDR block for the subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+  type        = string
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Map public IP on launch"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags to apply to the VPC and subnet"
+  type        = map(string)
+  default     = {}
+}
